07d3b57c86142c6252e232995eeef87bcf3a4a14
Muhammad Faiz Al Ghozy <muhammadfaizalghozi1@gmail.com>
2023-08-13 17:54:46 +0700

    fix: change base url

--- src/router.js
+++ src/router.js
@@ -2,7 +2,7 @@ const router = require("express")();
 const cheerio = require("cheerio");
 const { AxiosService } = require("./helper/axios_service");
 const { responseApi } = require("./helper/response_api");
-const baseUrl = "https://komikcast.io";
+const baseUrl = "https://komikcast.vip";
 
 router.get("/", (req, res) => {
   return res.status(200).json({
